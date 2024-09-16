8ea66ef10794566156bcca65746b813e4ff741ba
DPSS <dafnesan25@gmail.com>
2024-09-16 02:10:06 -0600

    Se crea el primer ejercicio

---
+++ src/Numeros.java
@@ -0,0 +1,14 @@
+
+public class Numeros {
+
+	public static void main(String[] args) {
+	
+		for (int x = 1; x <= 3; x++) {
+		for (int y = 1; y <= 3; y++) {
+			System.out.println(y);
+		}// for x
+		} //for y
+
+	}//while
+
+}//class números
